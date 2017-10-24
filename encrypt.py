0d7c5dd82f77767487773c3d3ff4738a5878c08e
Joseph Potter <Joseph Potter>
2017-10-23 18:07:25 -0600

    Added a loop to continue

--- encrypt.py
+++ encrypt.py
@@ -6,12 +6,19 @@ Last Edited: 10/23/2017
 Author: Joseph Potter
 Description: See README
 """
-Encrypter = encrypter()
-choice = input("Would you like to decrypt or encrypt?(e/d): ")
-Encrypter.get_key()
-if choice == 'e':
-    Encrypter.get_variables(input("Enter message: "),0)
-    print(Encrypter.encrypt())
-else:
-    Encrypter.get_variables(input("Enter message: "),2)
-    print(Encrypter.decrypt())
+Continue = True
+while Continue:
+    Encrypter = encrypter()
+    choice = input("Would you like to decrypt or encrypt?(e/d): ")
+    Encrypter.get_key()
+    if choice == 'e':
+        Encrypter.get_variables(input("Enter message: "),0)
+        print ("Cipher Text:")
+        print(Encrypter.encrypt())
+    else:
+        Encrypter.get_variables(input("Enter message: "),2)
+        print ("Plain Text:")
+        print(Encrypter.decrypt())
+    continue_choice = input("Would you like to keep going?(y/n)")
+    if continue_choice not in ['y', 'Y']:
+        Continue = False
